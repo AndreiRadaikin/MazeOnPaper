cd103af7e72ec2da691f31fc95ee3206aa23abfa
Bohdan <bvsol1998@gmail.com>
2019-03-13 01:55:31 +0200

    Added base folder tree for the game master

---
+++ components/request/interaction_request/interface/interaction_request.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <memory>
+
+namespace Request {
+class InteractionRequest{};
+typedef std::shared_ptr<InteractionRequest> InteractionRequestPtr;
+} // Request
---
+++ components/response/action_response/interface/action_response.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <memory>
+
+namespace Response {
+class ActionResponse {};
+typedef std::shared_ptr<ActionResponse> ActionResponsePtr;
+} // Response
