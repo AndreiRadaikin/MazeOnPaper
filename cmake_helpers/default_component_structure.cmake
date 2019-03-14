dfb1adf5a2073b61bf9b6831d7ea3201655a3da0
Bohdan <bvsol1998@gmail.com>
2019-03-14 12:45:13 +0200

    CMakeLists fixed

--- cmake_helpers/default_component_structure.cmake
+++ cmake_helpers/default_component_structure.cmake
@@ -1,7 +1,15 @@
+function(MakeInterfaseStructure)
 add_library(${PROJECT_NAME} INTERFACE)
 add_library(${PROJECT_NAME}::interface ALIAS ${PROJECT_NAME})
 
 target_include_directories(${PROJECT_NAME}
         INTERFACE "${CMAKE_CURRENT_LIST_DIR}/interface")
 
+if(LIBRARIES)
+ target_link_libraries(${PROJECT_NAME} INTERFACE ${LIBRARIES})
+endif()
+
+set(INTERFACE_NAME ${PROJECT_NAME})
 add_subdirectory(implementation)
+endfunction(MakeInterfaseStructure)
+
